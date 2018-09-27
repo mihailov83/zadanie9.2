4b3780db0bd895bc7454b97e4d067563796ad451
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-28 00:29:15 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ var allNames = femaleNames.concat(maleNames);
 var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1) {
-    allNames.push(newName)
+    allNames.push(newName);
 }
 
 console.log(allNames)
